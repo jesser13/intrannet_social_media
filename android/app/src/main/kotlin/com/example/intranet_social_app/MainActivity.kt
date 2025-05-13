278a5bc54f4504160b40e52be5adc19148300a0e
jesser13 <mhadhbijesser1@gmail.com>
2025-05-13 22:30:43 +0200

    commit 1

--- android/app/src/main/kotlin/com/example/intranet_social_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.intranet_social_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ intranet_app/android/app/src/main/kotlin/com/example/intranet_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.intranet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
